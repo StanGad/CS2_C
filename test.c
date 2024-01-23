5c8b1c0ecd6219cd8b08489bde4dbcbc925eeb61
Stanislas-Gadeceau <stanislasgadeceau@hotmail.com>
2024-01-23 17:11:29 +0100

    vtest mathieu

--- test.c
+++ test.c
@@ -1,4 +1,27 @@
+#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+
+void* mymalloc(size_t size){
+
+    void* a = sbrk(size);
+
+    if(a == (void *) -1 ){
+        printf("FAILURE");
+        return (void*)-1;
+    }else{
+        return a;
+    }
+
+    
+
+    // printf("pointer address %p", a);
+    // a = sbrk(0);
+    // printf("pointer address %p", a);
+
+    // return EXIT_SUCCESS;
+}
+
 
 void free(void *ptr) {
     if (ptr == NULL) {
@@ -10,4 +33,11 @@ void free(void *ptr) {
     size_t block_size = *((size_t *)block_start);
     
     brk(block_start - block_size);
+}
+
+void main(){
+
+    char* a = mymalloc(sizeof(char));
+    *a = 's';
+    printf("My char %c", *a);
 }
