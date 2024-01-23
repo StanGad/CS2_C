dcb1adf9e5d7405816dc6f8868ccddbc80ffe87d
molerae <emmawr02@hotmail.com>
2024-01-23 16:12:56 +0100

    test

--- test.c
+++ test.c
@@ -1,2 +1,13 @@
-int main(void argv[]){dedzdzdz
+#include <unistd.h>
+
+void free(void *ptr) {
+    if (ptr == NULL) {
+        return;
+    }
+
+    char *block_start = (char *)ptr - sizeof(size_t);
+
+    size_t block_size = *((size_t *)block_start);
+    
+    brk(block_start - block_size);
 }
